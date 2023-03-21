560c398bb7c5d54bf40cd30fa6ab897ae014d0f6
oliver <oscgm98@gmail.com>
2023-03-21 17:08:28 +0100

    Updated ontology + Compositional Wikidata Questions benchmark
    ## Changes
    * Updated QADO ontology from 0.1.0 to 0.2.0
    * Added new benchmark: Compositional Wikidata Questions (https://github.com/coastalcph/seq2sparql)
    * Providing separated TTL files foreach benchmark

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
-curl --silent --output /dev/null -X DELETE "$EXPORT_DB/admin/databases/qado"
-curl --silent --output /dev/null -X POST -F root="{\"dbname\": \"qado\"}" "$EXPORT_DB/admin/databases"
-tx=$(curl --silent -X POST "$EXPORT_DB/qado/transaction/begin")
-curl --silent --output /dev/null -X POST -H "Content-Type: text/turtle" --data-binary "@qado.ttl" "$EXPORT_DB/qado/${tx}/add?graph-uri=default"
-curl --silent --output /dev/null -X POST "$EXPORT_DB/qado/transaction/commit/$tx"
+curl -k --silent --output /dev/null -X DELETE "$EXPORT_DB/admin/databases/qado"
+curl -k --silent --output /dev/null -X POST -F root="{\"dbname\": \"qado\"}" "$EXPORT_DB/admin/databases"
+tx=$(curl -k --silent -X POST "$EXPORT_DB/qado/transaction/begin")
+curl -k --silent --output /dev/null -X POST -H "Content-Type: text/turtle" --data-binary "@qado.ttl" "$EXPORT_DB/qado/${tx}/add?graph-uri=default"
+curl -k --silent --output /dev/null -X POST "$EXPORT_DB/qado/transaction/commit/$tx"
