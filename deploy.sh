268ccd38f571c0a7e821ae45241b8180987e85e1
oliver <oscgm98@gmail.com>
2023-08-22 15:53:54 +0200

    Fixed display of the progress bar for QADO query validation

--- deploy.sh
+++ deploy.sh
@@ -92,7 +92,7 @@ function insertDataIntoDb() {
 
 function stopDeployer() {
   echo "Stopping deployment tools..."
-  docker-compose logs graph_db
+  docker-compose logs graphdb
   docker-compose down
   docker container rm "$(docker ps -a -q --filter ancestor=sparql-analyser:latest)" > /dev/null
   docker volume rm sparql-analyse > /dev/null
